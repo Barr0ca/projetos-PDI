884001c2ac81bab61951ecbc04ad621bb0ace3bc
Ian Galvão <137799663+Barr0ca@users.noreply.github.com>
2026-05-05 08:00:27 -0300

    fix: Correção do código para funcionamento do carregamento da imagem

--- aula05/lab05.py
+++ aula05/lab05.py
@@ -9,24 +9,24 @@ img = cv2.imread("lobo.jpg")
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Imagem equalizada
-img_equalized = cv2.equalizeHist(img)
+img_equalized = cv2.equalizeHist(img_gray)
 
 # Imagem em tons de cinza
-plt.subplot(121), plt.imshow(img_gray, cmap="gray")
+plt.subplot(2, 2, 1), plt.imshow(img_gray, cmap="gray")
 plt.axis("off")
 plt.title("Grayscale")
 
 # Gráfico do histograma (PIXELS x INTENSIDADE)
-plt.subplot(122), plt.hist(img_gray.ravel(), 256, [0, 256], color="k")
+plt.subplot(2, 2, 2), plt.hist(img_gray.ravel(), 256, [0, 256], color="k")
 plt.title("Gray Histogram")
 
 # Imagem em tons de cinza equalizada
-plt.subplot(123), plt.imshow(img_equalized, cmap="gray")
+plt.subplot(2, 2, 3), plt.imshow(img_equalized, cmap="gray")
 plt.axis("off")
 plt.title("Equalized")
 
 # Gráfico do histograma equalizado (PIXELS x INTENSIDADE)
-plt.subplot(124), plt.hist(img_equalized.ravel(), 256, [0, 256], color="k")
+plt.subplot(2, 2, 4), plt.hist(img_equalized.ravel(), 256, [0, 256], color="k")
 plt.title("Equalize Histogram")
 
 # for i, c in enumerate(('r','g','b')):
